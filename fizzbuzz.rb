51336cfafd59eea06433d1153df9e01c8c8cf5b7
Naoki Kanyama <j1411@matsue-ct.ac.jp>
2017-11-21 09:44:15 +0900

    fizzbuzz分岐と表示作成

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,11 @@
 (1..100).each{|i|
-
+	if i % 3 == 0 && i % 5 == 0
+		print "FizzBuzz\n"
+	else if i % 3 == 0
+		print "Fizz\n"
+	else if i % 5 == 0
+		print "Buzz\n"
+	else
+		print i + "\n"
+	end
 }
